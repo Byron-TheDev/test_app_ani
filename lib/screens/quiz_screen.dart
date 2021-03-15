4b39e90c8de1dfa851adc0b609a9b28ea81c8dd9
ByronLax <74532262+Byron-TheDev@users.noreply.github.com>
2021-03-15 18:14:04 +0530

    Update quiz_screen.dart

--- lib/screens/quiz_screen.dart
+++ lib/screens/quiz_screen.dart
@@ -52,8 +52,7 @@ class _QuizPageState extends State<QuizPage>
   void buttonFunction(bool trueOrFalse) {
     setState(() {
       if (index >= questionsAns.length) {
-       
-      Navigator.pushReplacementNamed(context, ScorePage.route,
+       Navigator.pushReplacementNamed(context, ScorePage.route,
             arguments: score);
       } else if (questionsAns[index]['ans'] == trueOrFalse) {
         index++;
@@ -61,7 +60,7 @@ class _QuizPageState extends State<QuizPage>
         controller.forward();
         score += 1;
       } else if (questionsAns[index]['ans'] != trueOrFalse) {
-        score < = 0? score = 0 : score -= 1;
+        score <= 0? score = 0 : score -= 1;
         Navigator.pushReplacementNamed(context, ScorePage.route,
             arguments: score);
       }
